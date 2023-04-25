e07b09b23f081ed7b509a965843fc46353adcf30
Jule Pohlhausen <pohlhausenjule@gmail.com>
2023-04-25 08:00:01 +0000

    input options for snr and scaling

--- mms_msg/databases/meeting/vpc_meeting.py
+++ mms_msg/databases/meeting/vpc_meeting.py
@@ -31,7 +31,7 @@ OVERLAP_SETTINGS = {
 
 def AnechoicVPCMeeting(source_json_path=database_jsons / 'vpc.json',
                                duration=120 * 16000, overlap_conditions='medium_ov', num_speakers=(5, 6, 7, 8),
-                               within_chapter=True):
+                               snr_min_max=(20, 30), max_weight=5, within_chapter=True):
     """
     Meetings based on the VPC test data.
 
@@ -42,6 +42,9 @@ def AnechoicVPCMeeting(source_json_path=database_jsons / 'vpc.json',
         overlap_conditions: Specifies the overlap scenario, either via pre-defined scenarios or custom values
             either str or dict of overlap settings
         num_speakers: Number of speakers per meeting. Any permitted number of speakers needs to be listed.
+        snr_min_max: Gives each utterance a different SNR, sampled from a uniform distribution between `snr_min` and `snr_max`
+        max_weight: Gives each utterance a different log_weight, sampled from a uniform distribution between 
+            0 and `max_weight` and mean-normalized
         within_chapter: If true, ensures that utterances of each speaker in a meeting are sampled from
             the same chapter such that the acoustic properties (e.g. recording device) don't differ
     Returns:
@@ -59,9 +62,11 @@ def AnechoicVPCMeeting(source_json_path=database_jsons / 'vpc.json',
     source_database = VPC(source_json_path, scenario_key=scenario_key)
     overlap_sampler = UniformOverlapSampler(**overlap_conditions)
     meeting_sampler = MeetingSampler(duration, overlap_sampler=overlap_sampler)
+    snr_sampler = UniformSNRSampler(snr_min_max[0], snr_min_max[1])
+    scaling_sampler = UniformScalingSampler(max_weight=max_weight)
     return AnechoicMeetingDatabase(source_database=source_database,
                                    num_speakers=num_speakers,
                                    meeting_sampler=meeting_sampler,
-                                   scaling_sampler=UniformScalingSampler(5),
-                                   snr_sampler=UniformSNRSampler(20, 30),
+                                   scaling_sampler=scaling_sampler,
+                                   snr_sampler=snr_sampler,
                                    )
