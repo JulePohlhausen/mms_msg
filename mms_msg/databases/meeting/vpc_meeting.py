2a944bf7dd8b8feb87359191ff1347c24ba5061a
Jule Pohlhausen <pohlhausenjule@gmail.com>
2023-05-08 10:26:23 +0000

    update ti unique speaker combis

--- mms_msg/databases/meeting/vpc_meeting.py
+++ mms_msg/databases/meeting/vpc_meeting.py
@@ -4,7 +4,6 @@ from mms_msg.sampling.environment.scaling import UniformScalingSampler
 from mms_msg.sampling.pattern.meeting import MeetingSampler
 from mms_msg.sampling.pattern.meeting.overlap_sampler import UniformOverlapSampler
 from mms_msg.sampling.environment.noise import UniformSNRSampler
-from mms_msg.sampling.source_composition import sample_utterance_composition
 from mms_msg.databases.single_speaker.vpc import VPC
 from .database import AnechoicMeetingDatabase
 
@@ -62,11 +61,10 @@ def AnechoicVPCMeeting(source_json_path=database_jsons / 'vpc.json',
         scenario_key = None
     source_database = VPC(source_json_path, scenario_key=scenario_key)
     overlap_sampler = UniformOverlapSampler(**overlap_conditions)
-    meeting_sampler = MeetingSampler(duration, overlap_sampler=overlap_sampler)
+    meeting_sampler = MeetingSampler(duration, overlap_sampler=overlap_sampler, sample_unique=True)
     snr_sampler = UniformSNRSampler(snr_min_max[0], snr_min_max[1])
     scaling_sampler = UniformScalingSampler(max_weight=max_weight)
     composition_sampler = 'unique'
-    #composition_sampler = sample_utterance_composition
     return AnechoicMeetingDatabase(source_database=source_database,
                                    num_speakers=num_speakers,
                                    meeting_sampler=meeting_sampler,
